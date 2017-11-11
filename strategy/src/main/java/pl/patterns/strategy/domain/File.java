193fe5438629f8920c9c318f573d09b67ca13601
Rafal Dworznik <rapiernik@gmail.com>
2017-11-11 19:05:18 +0100

    implement simple Observer Design Pattern

--- strategy/src/main/java/pl/patterns/strategy/domain/File.java
+++
@@ -1,6 +0,0 @@
-package pl.patterns.strategy.domain;
-
-public class File {
-
-    //content of File class
-}
--- strategy/src/main/java/pl/patterns/strategy/strategy/CompressionStrategy.java
+++
@@ -1,9 +0,0 @@
-package pl.patterns.strategy.strategy;
-
-import pl.patterns.strategy.domain.File;
-
-import java.util.List;
-
-public interface CompressionStrategy {
-    void compressFiles(List<File> files);
-}
